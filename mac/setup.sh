a8b1f61b2d08ba2219fbb8435505d9515d35d548
Alex Kaplan <kaplan2539@gmail.com>
2017-06-05 17:47:29 -0700

    add deb and dmg generation

---
+++ mac/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+brew_install() 
+{
+    PACKAGE=$1
+    if [[ -z "$PACKAGE" ]]; then
+        return 1;
+    fi
+
+    brew ls --versions $PACKAGE > /dev/null || brew install $PACKAGE || exit 1
+}
+
+brew_install cmake
+brew_install qt5
+brew_install librsvg
+brew_install ninja
+brew_install gettext
+brew link --force gettext
